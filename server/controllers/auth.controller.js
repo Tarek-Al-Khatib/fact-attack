b669341e553baa91cdafbebd15cbe56de4d4a67f
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-06 20:34:00 +0200

    Added login function to the auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,44 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { defaultQuizzes } from "../defaults/quizzes.defaults.js";
 import jwt from "jsonwebtoken";
 
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({
+      username,
+    });
+
+    if (!user) {
+      return res.status(404).send({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const check = await bcrypt.compare(password, user.password);
+
+    if (!check) {
+      return res.status(400).send({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = await jwt.sign({ userId: user.id }, "secret");
+
+    return res.status(200).send({ user, token });
+  } catch (error) {
+    console.log(error.message);
+
+    return res.status(500).send({
+      message: "Something went wrong",
+    });
+  }
+};
+
 export const register = async (req, res) => {
-  const { username, password, age } = req.body;
+  const { username, password } = req.body;
 
   try {
     if (!username || !password) {
@@ -16,8 +51,8 @@ export const register = async (req, res) => {
 
     const user = await User.create({
       username,
-      age,
       password: hashed,
+      quizzes: defaultQuizzes,
     });
 
     return res.json(user);
