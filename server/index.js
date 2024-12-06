a950ffa8f5e488ffd4e61ea24ffe294416f9e3ae
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-06 20:11:37 +0200

    Added some changes so the application can run

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { connect } from "mongoose";
 
 const app = express();
 
@@ -7,7 +8,7 @@ app.use(express.json());
 app.listen(8080, async () => {
   console.log("Server running on port 8080");
   try {
-    await connect("mongodb://localhost:27017/introdb");
+    await connect("mongodb://localhost:27017/factattackdb");
 
     console.log("Connected to database");
   } catch (error) {
