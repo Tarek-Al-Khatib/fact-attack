e104b738390f4eb61dba28198161de87b4dda612
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-06 20:45:44 +0200

    Fixed some import errors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { connect } from "mongoose";
-
+import { authRouter } from "./routes/auth.routes.js";
 const app = express();
 
 app.use(express.json());
+app.use("/auth", authRouter);
 
 app.listen(8080, async () => {
   console.log("Server running on port 8080");
